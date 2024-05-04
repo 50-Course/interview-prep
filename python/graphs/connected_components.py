af5887693f1e9c0f3c3f4d65721022df8a2f7f9c
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-05-04 08:21:10 +0100

    feat: connected components with DFS (iterative)

--- python/graphs/connected_components.py
+++ python/graphs/connected_components.py
@@ -2,7 +2,25 @@
 # undirected graph, The function should return the num of
 # connected componenets in the graph
 def connected_graphs(graph):
-    pass
+    count = 0
+    stack = []
+    visited = set()
+
+    for node in graph:
+        if node not in visited:
+            stack.append(node)
+            visited.add(node)
+
+            while stack:
+                current = stack.pop()
+
+                for neighbor in graph[current]:
+                    if neighbor not in visited:
+                        stack.append(neighbor)
+                        visited.add(neighbor)
+            count += 1
+    return count
+
 
 if __name__ == '__main__':
     graph = {
