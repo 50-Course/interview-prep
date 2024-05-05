9450aab486f5ed04b2db5ea5e1116afe0992a525
50-Course <erioluwaadeodu@my.uopeople.edu>
2024-05-05 15:30:55 +0100

    problem: finding the size of the largest component in a graph

---
+++ python/largest_component.py
@@ -0,0 +1,17 @@
+# Write a function that takes in the adjacency list of an
+# undirected graph.
+# The function should return the size of the largest connected
+# component in the graph
+
+def largest_component(graph):
+    """
+    Returns:
+        The size of the larrgest connected component
+    in the given graph.
+    """
+    pass
+
+
+if __name__ == '__main__':
+    print(largest_component(graph))
+
